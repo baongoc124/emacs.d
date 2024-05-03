b65d9a9b9fd41f5b96044a11ba79899adab11b29
Bao Ngoc <baongoc124@gmail.com>
2024-05-03 09:36:47 +0900

    add tree-sitter

--- init.el
+++ init.el
@@ -1167,8 +1167,8 @@ Version: 2015-10-01"
   :config
   (setq atomic-chrome-buffer-open-style 'full)
   (setq atomic-chrome-url-major-mode-alist
-        '((".*\\.wix\\.com" . js-mode)
-          ("leetcode\\.com" . python-mode)))
+        '((".*\\.wix\\.com" . js-ts-mode)
+          ("leetcode\\.com" . python-ts-mode)))
   (atomic-chrome-start-server))
 
 (use-package eglot
@@ -1383,6 +1383,34 @@ Version: 2015-10-01"
 (add-hook 'projectile-rails-mode-hook
           (lambda () (setq zeal-at-point-docset "ruby,rails")))
 (add-to-list 'zeal-at-point-mode-alist '(c++-mode . ""))
+
+
+;; better language grammar
+(use-package treesit
+  :ensure nil
+  :config
+  (setq treesit-language-source-alist
+        '((bash "https://github.com/tree-sitter/tree-sitter-bash")
+          (cmake "https://github.com/uyha/tree-sitter-cmake")
+          (css "https://github.com/tree-sitter/tree-sitter-css")
+          (elisp "https://github.com/Wilfred/tree-sitter-elisp")
+          (go "https://github.com/tree-sitter/tree-sitter-go")
+          (html "https://github.com/tree-sitter/tree-sitter-html")
+          (javascript "https://github.com/tree-sitter/tree-sitter-javascript" "master" "src")
+          (json "https://github.com/tree-sitter/tree-sitter-json")
+          (make "https://github.com/alemuller/tree-sitter-make")
+          (markdown "https://github.com/ikatyang/tree-sitter-markdown")
+          (python "https://github.com/tree-sitter/tree-sitter-python")
+          (toml "https://github.com/tree-sitter/tree-sitter-toml")
+          (tsx "https://github.com/tree-sitter/tree-sitter-typescript" "master" "tsx/src")
+          (typescript "https://github.com/tree-sitter/tree-sitter-typescript" "master" "typescript/src")
+          (yaml "https://github.com/ikatyang/tree-sitter-yaml"))))
+
+
+(use-package treesit-auto
+  :config
+  (global-treesit-auto-mode))
+
 ;;   ____
 ;;  / ___| _     _
 ;; | |   _| |_ _| |_
@@ -1431,18 +1459,20 @@ Version: 2015-10-01"
 (use-package anaconda-mode
   :hook
   (python-mode . anaconda-mode)
-  (python-mode . anaconda-eldoc-mode))
+  (python-mode . anaconda-eldoc-mode)
+  (python-ts-mode . anaconda-mode)
+  (python-ts-mode . anaconda-eldoc-mode))
 
 (use-package company-anaconda
   :after (eglot company)
   :config
   (add-to-list 'company-backends '(company-anaconda :with company-capf))
   (add-hook 'eglot-managed-mode-hook
-            '(lambda ()
-               (when (and (member major-mode '(python-mode python-ts-mode))
-                          (eglot-managed-p))
-                      (anaconda-mode -1)
-                      (anaconda-eldoc-mode -1)))))
+            #'(lambda ()
+                (when (and (member major-mode '(python-mode python-ts-mode))
+                           (eglot-managed-p))
+                  (anaconda-mode -1)
+                  (anaconda-eldoc-mode -1)))))
 
 (use-package vimrc-mode
   :defer t
