597d66eca17efb9386e4e5e0e6f79cb1a75b9c7d
Bao Ngoc <baongoc124@gmail.com>
2024-05-05 22:33:35 +0900

    persistent undo-tree

--- init.el
+++ init.el
@@ -88,10 +88,17 @@
 
 
 (use-package undo-tree
+  :demand t
   :bind (:map ngoc-prefix
          ("u" . undo-tree-visualize))
   :diminish undo-tree-mode
   :config
+  (setq undo-tree-auto-save-history t)
+  (setq undo-tree-enable-undo-in-region nil) ;; disabled because it's buggy
+  (setq undo-tree-history-directory-alist
+        `(("." . ,(file-name-as-directory (file-name-concat user-emacs-directory
+                                                           "cache"
+                                                           "undo-history")))))
   (global-undo-tree-mode))
 
 
