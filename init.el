ad20b84c406deb326f0bf53aeef8890d0774799c
Bao Ngoc <baongoc124@gmail.com>
2024-05-06 06:23:40 +0900

    add fill-function-arguments

--- init.el
+++ init.el
@@ -1064,6 +1064,34 @@ official one in emms.el to return filename only (no path)
 
 
 
+;;                                             _     _ _     _
+;;   __ _ _ __ __ _ _   _ _ __ ___   ___ _ __ | |_  | (_)___| |_
+;;  / _` | '__/ _` | | | | '_ ` _ \ / _ \ '_ \| __| | | / __| __|
+;; | (_| | | | (_| | |_| | | | | | |  __/ | | | |_  | | \__ \ |_
+;;  \__,_|_|  \__, |\__,_|_| |_| |_|\___|_| |_|\__| |_|_|___/\__|
+;;            |___/
+(use-package fill-function-arguments
+  :bind ("C-h C-q" . ngoc/fill-function-arguments-dwim)
+  :config
+  (setq fill-function-arguments-fall-through-to-fill-paragraph nil)
+  (setq fill-function-arguments-indent-after-fill t)
+  (setq fill-function-arguments-trailing-separator t)
+
+  ;; without prefix argument, do default behavior
+  ;; with prefix argument, do compact version
+  ;; and DO NOTHING if not inside a parenthesis
+  (defun ngoc/fill-function-arguments-dwim (&optional arg)
+    (interactive "P")
+    (if (<= (nth 0 (syntax-ppss)) 0)
+        (message "Not inside a parenthesis")
+      (if (not arg)
+          (fill-function-arguments-dwim)
+        (let ((fill-function-arguments-trailing-separator nil)
+              (fill-function-arguments-first-argument-same-line t)
+              (fill-function-arguments-last-argument-same-line t))
+          (fill-function-arguments-dwim))))))
+
+
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;                              IMPATIENT MODE
