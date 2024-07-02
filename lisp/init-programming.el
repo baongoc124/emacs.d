de98e9a364359717cccee64026668c1731cdf17a
Bao Ngoc <baongoc124@gmail.com>
2024-07-03 04:36:12 +0900

    add flymake-diagnostic-at-point

--- lisp/init-programming.el
+++ lisp/init-programming.el
@@ -6,6 +6,14 @@
   (global-set-key (kbd "C-q") #'quickselect-mark)
   (global-set-key (kbd "C-'") #'quoted-insert))
 
+
+(use-package flymake)
+(use-package flymake-diagnostic-at-point
+  :after flymake
+  :config
+  (add-hook 'flymake-mode-hook #'flymake-diagnostic-at-point-mode))
+
+
 ;;                                             _     _ _     _
 ;;   __ _ _ __ __ _ _   _ _ __ ___   ___ _ __ | |_  | (_)___| |_
 ;;  / _` | '__/ _` | | | | '_ ` _ \ / _ \ '_ \| __| | | / __| __|
