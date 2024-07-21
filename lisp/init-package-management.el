be792a06080a63f26f234a4937a6bc3c9905a615
Bao Ngoc <baongoc124@gmail.com>
2024-07-22 08:18:58 +0900

    add nord-lightt to theme path

--- lisp/init-package-management.el
+++ lisp/init-package-management.el
@@ -3,6 +3,8 @@
 (add-to-list 'load-path "~/.emacs.d/lisp/quickselect")
 (add-to-list 'load-path "~/.emacs.d/lisp/netlogo-mode/emacs")
 
+(add-to-list 'custom-theme-load-path "~/.emacs.d/lisp/nord-lightt/")
+
 
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
