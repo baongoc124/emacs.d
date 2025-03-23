fbae31109def31484554b7bab5daa13371eec355
Ngoc Nguyen <baongoc124@gmail.com>
2025-03-23 23:40:54 +0900

    focus ripgrep window when it appears

--- lisp/init-search.el
+++ lisp/init-search.el
@@ -1,5 +1,11 @@
 (use-package rg
   :config
-  (rg-enable-default-bindings (kbd "<leader>/")))
+  (rg-enable-default-bindings (kbd "<leader>/"))
+
+  ;; focus the rg buffer after starting the search
+  (add-to-list
+   'display-buffer-alist
+   '("\\*rg\\*" . (nil . ((body-function . select-window)))))
+  )
 
 (provide 'init-search)
