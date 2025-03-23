7e5a1439937d5ec58b40cdaff4c6fd9cd557c41d
Ngoc Nguyen <baongoc124@gmail.com>
2025-03-23 23:36:55 +0900

    switch to zenburn theme

--- lisp/init-core.el
+++ lisp/init-core.el
@@ -64,13 +64,53 @@
 ;;   (setq doom-themes-treemacs-theme "doom-atom") ; use "doom-colors" for less minimal icon theme
 ;;   (doom-themes-treemacs-config)
 ;;   ;; Corrects (and improves) org-mode's native fontification.
-;;   (doom-themes-org-config))
-)
+  (doom-themes-org-config)
+
+  (load-theme 'doom-zenburn t)
+
+
+  ;; custom settings for doom-zenburn theme
+  (set-face-attribute 'shadow nil :foreground "grey70")
+  (set-face-attribute 'trailing-whitespace nil :background "red")
+  (eval-after-load 'ivy
+    '(progn
+       (set-face-attribute 'ivy-minibuffer-match-face-1 nil :foreground "#CC9393")
+       (set-face-attribute 'ivy-current-match nil :background "#2b2b2b")
+       ))
+
+  ;; scale taken from emacs-zenburn theme
+  (eval-after-load 'org
+    '(progn
+       (set-face-attribute 'org-document-title nil :height 1.3)
+       (set-face-attribute 'org-level-1 nil :height 1.3)
+       (set-face-attribute 'org-level-2 nil :height 1.2)
+       (set-face-attribute 'org-level-3 nil :height 1.15)
+       (set-face-attribute 'org-level-4 nil :height 1.1)
+       )
+    )
+
+  (eval-after-load 'ace-window
+    '(progn
+       (set-face-attribute 'aw-leading-char-face nil :weight 'bold :height 1.2)
+       )
+    )
+
+  (set-face-attribute 'mode-line nil :height 1.1 :box '(:line-width (1 . 4) :color nil :style flat-button))
+  (set-face-attribute 'region nil :extend t :background "#8c5353")
+
+  )
+
+;; (use-package zenburn-theme
+;;   :config
+;;   (load-theme 'zenburn t))
 
 (defun ngoc/load-nord-lightt ()
   (interactive)
   (save-buffer)
-  (load-theme 'nord-lightt t))
+  (load-theme 'doom-zenburn t))
+
+;; (load-theme 'nord-lightt t)
+;; ;; (load-theme 'modus-operandi-tinted t)
 
 (add-hook 'window-setup-hook
           #'(lambda ()
@@ -133,9 +173,6 @@ mouse-3: Toggle minor modes"
 ;; DONE order all minor modes in modeline
 ;; FIXME find a better way to reorder minor mode name on modeline
 (add-hook 'after-load-functions 'ngoc/sort-minor-modes)
-
-
-(load-theme 'nord-lightt t)
 
 
 (defun rmrf/setup-frame (&optional frame)
