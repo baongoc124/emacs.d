cb6b9276fc5db1542223d877ea0a8f5db403c38e
Ngoc Nguyen <baongoc124@gmail.com>
2025-04-13 05:11:04 +0900

    add codeium

--- lisp/init-eglot.el
+++ lisp/init-eglot.el
@@ -1,5 +1,7 @@
+(use-package cape)
+
 (use-package eglot
-  :after (transient warnings)
+  :after (transient warnings cape)
   :init
   ;; do not display this warning from
   ;; jsonrpc ‘:events-buffer-scrollback-size’ deprecated. Use ‘events-buffer-config’.
@@ -11,6 +13,30 @@
   (setq eglot-extend-to-xref t)
   (setq eglot-autoshutdown t)
   (setq eglot-report-progress t)
+
+  (defun my/my-eglot-setup ()
+    (when (eglot-managed-p)
+      (setq company-backends (list
+                              '(company-capf company-dabbrev-code company-files :separate)))
+      )
+
+
+    ;; https://github.com/company-mode/company-mode/discussions/1442
+    (defun my/codeium-completion-at-point ()
+      (when (and (or (null (thing-at-point 'symbol))
+                     (< (length (thing-at-point 'symbol)) company-minimum-prefix-length))
+                 (not (company-explicit-action-p)))
+        (codeium-completion-at-point)))
+
+    ;; (add-hook 'completion-at-point-functions #'my/codeium-completion-at-point -10 t)
+    (add-hook 'completion-at-point-functions (cape-capf-nonexclusive 'eglot-completion-at-point) 0 t)
+    (add-hook 'completion-at-point-functions #'codeium-completion-at-point 10 t)
+
+    (when (eq major-mode 'terraform-mode)
+      (setq completion-at-point-functions '(codeium-completion-at-point eglot-completion-at-point t))
+      )
+    )
+  (add-hook 'eglot-managed-mode-hook #'my/my-eglot-setup)
 
   ;; python
   (add-to-list 'eglot-server-programs
