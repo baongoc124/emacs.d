8186068ddaa7c6e365ba36a7d6095dabddb2853a
Ngoc Nguyen <baongoc124@gmail.com>
2025-08-27 04:22:52 +0900

    add visual-regexp

--- lisp/init-search.el
+++ lisp/init-search.el
@@ -8,4 +8,21 @@
    '("\\*rg\\*" . (nil . ((body-function . select-window)))))
   )
 
+
+(use-package visual-regexp-steroids
+  :bind (:map evil-normal-state-map
+              ("gr" . vr/query-replace))
+  :config
+  (setq vr/match-separator-use-custom-face t)
+  (set-face-attribute 'vr/match-separator-face nil :background 'unspecified :inherit 'default)
+
+  ;; auto save current undo-tree state so that i can jump back to before I started query-replace
+  ;; TODO: might be good to use for other commands as well?
+  (defun my/save-current-undo-tree-state ()
+    (interactive)
+    (undo-tree-save-state-to-register ?@)
+    )
+  (add-hook 'vr/initialize-hook #'my/save-current-undo-tree-state)
+  )
+
 (provide 'init-search)
