897023bc0dbdf6da8223224315057e3314a7a2ad
Ngoc Nguyen <baongoc124@gmail.com>
2025-08-30 07:02:32 +0900

    remove codeium; add minuet

--- lisp/init-ai.el
+++ lisp/init-ai.el
@@ -81,76 +81,121 @@ When responding to me:
   )
 
 
-(use-package codeium
-    :after eglot
-    ;; if you use straight
-    ;; :straight '(:type git :host github :repo "Exafunction/codeium.el")
-    ;; otherwise, make sure that the codeium.el file is on load-path
-    :vc (:fetcher github :repo "Exafunction/codeium.el")
+(use-package minuet
+  :vc (:fetcher github :repo "milanglacier/minuet-ai.el")
+  :diminish minuet-auto-suggestion-mode
+  :bind
+  (
+   ;; ("M-y" . #'minuet-complete-with-minibuffer) ;; use minibuffer for completion
+   ("C-c SPC" . #'minuet-show-suggestion) ;; use overlay for completion
+   ;; ("C-c m" . #'minuet-configure-provider)
+   :map minuet-active-mode-map
+   ;; These keymaps activate only when a minuet suggestion is displayed in the current buffer
+   ("M-p" . #'minuet-previous-suggestion) ;; invoke completion or cycle to next completion
+   ("M-n" . #'minuet-next-suggestion) ;; invoke completion or cycle to previous completion
+   ("M-SPC" . #'minuet-accept-suggestion) ;; accept whole completion
+   ;; Accept the first line of completion, or N lines with a numeric-prefix:
+   ;; e.g. C-u 2 M-a will accepts 2 lines of completion.
+   ("M-a" . #'minuet-accept-suggestion-line)
+   ("M-e" . #'minuet-dismiss-suggestion))
 
-    :init
-    ;; use globally
-    ;; or on a hook
-    ;; (add-hook 'python-mode-hook
-    ;;     (lambda ()
-    ;;         (setq-local completion-at-point-functions '(codeium-completion-at-point))))
+  :init
+  ;; if you want to enable auto suggestion.
+  ;; Note that you can manually invoke completions without enable minuet-auto-suggestion-mode
+  (add-hook 'prog-mode-hook #'minuet-auto-suggestion-mode)
 
-    ;; if you want multiple completion backends, use cape (https://github.com/minad/cape):
-    ;; (add-hook 'python-mode-hook
-    ;;     (lambda ()
-    ;;         (setq-local completion-at-point-functions
-    ;;             (list (cape-capf-super #'codeium-completion-at-point #'lsp-completion-at-point)))))
-    ;; an async company-backend is coming soon!
+  :config
+  ;; You can use M-x minuet-configure-provider to interactively configure provider and model
+  ;; (setq minuet-provider 'openai-fim-compatible)
 
-    ;; codeium-completion-at-point is autoloaded, but you can
-    ;; optionally set a timer, which might speed up things as the
-    ;; codeium local language server takes ~0.2s to start up
-    ;; (add-hook 'emacs-startup-hook
-    ;;  (lambda () (run-with-timer 0.1 nil #'codeium-init)))
+  (setq minuet-request-timeout 5)
+  (setq minuet-auto-suggestion-throttle-delay 1.0) ;; minimum time in secs between auto suggestions
+  (setq minuet-auto-suggestion-debounce-delay 1.0) ;; delay in secs to show suggestions after stopping typing
 
-    ;; :defer t ;; lazy loading, if you want
-    :config
+  (defvar my-key-index 0)
+  (defvar my-keys (split-string (m/get-password "gemini.google.com" "apikeys") ","))
+  (setq my-key-index (random (length my-keys)))
 
-    (defun my/enable-codeium ()
-         (add-to-list 'completion-at-point-functions 'codeium-completion-at-point)
-         )
+  (defun my/rotate-keys ()
+    "Rotate through keys in round-robin fashion."
+    (interactive)
+    ;; show a message with index of current key
+    (message "Minuet: Using key %d" my-key-index)
+    (let ((key (nth my-key-index my-keys)))
+      (setq my-key-index (mod (1+ my-key-index) (length my-keys)))
+      key))
 
-    (with-eval-after-load 'nix-mode
-      (add-hook 'nix-mode-hook #'my/enable-codeium)
-        )
-
-    (setq use-dialog-box nil) ;; do not use popup boxes
-
-    ;; if you don't want to use customize to save the api-key
-    ;; (setq codeium/metadata/api_key "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx")
-
-    ;; get codeium status in the modeline
-    (setq codeium-mode-line-enable
-        (lambda (api) (not (memq api '(CancelRequest Heartbeat AcceptCompletion)))))
-    ;; (add-to-list 'mode-line-format '(:eval (car-safe codeium-mode-line)) t)
-    ;; alternatively for a more extensive mode-line
-    (add-to-list 'mode-line-format '(-50 "" codeium-mode-line) t)
-
-    ;; use M-x codeium-diagnose to see apis/fields that would be sent to the local language server
-    (setq codeium-api-enabled
-        (lambda (api)
-            (memq api '(GetCompletions Heartbeat CancelRequest GetAuthToken RegisterUser auth-redirect AcceptCompletion))))
-    ;; you can also set a config for a single buffer like this:
-    ;; (add-hook 'python-mode-hook
-    ;;     (lambda ()
-    ;;         (setq-local codeium/editor_options/tab_size 4)))
-
-    ;; ;; You can overwrite all the codeium configs!
-    ;; ;; for example, we recommend limiting the string sent to codeium for better performance
-    ;; (defun my-codeium/document/text ()
-    ;;     (buffer-substring-no-properties (max (- (point) 3000) (point-min)) (min (+ (point) 1000) (point-max))))
-    ;; ;; if you change the text, you should also change the cursor_offset
-    ;; ;; warning: this is measured by UTF-8 encoded bytes
-    ;; (defun my-codeium/document/cursor_offset ()
-    ;;     (codeium-utf8-byte-length
-    ;;         (buffer-substring-no-properties (max (- (point) 3000) (point-min)) (point))))
-    ;; (setq codeium/document/text 'my-codeium/document/text)
-    ;; (setq codeium/document/cursor_offset 'my-codeium/document/cursor_offset)
+  (defun my/get-openrouter-api-key ()
+    (m/get-password "openrouter.ai" "apikey")
     )
+
+  (setq minuet-show-error-message-on-minibuffer t)
+  (setq minuet-provider 'openai-fim-compatible)
+  ;; (plist-put minuet-openai-compatible-options :end-point "https://openrouter.ai/api/v1/chat/completions")
+  ;; (plist-put minuet-openai-compatible-options :api-key #'my/get-openrouter-api-key)
+  ;; (plist-put minuet-openai-compatible-options :model "deepseek/deepseek-chat-v3-0324:free")
+
+  ;; Prioritize throughput for faster completion
+  ;; (minuet-set-optional-options minuet-openai-compatible-options :provider '(:sort "throughput"))
+  ;; (minuet-set-optional-options minuet-openai-compatible-options :max_tokens 1024)
+  ;; (minuet-set-optional-options minuet-openai-compatible-options :top_p 0.9)
+
+  ;; (minuet-set-optional-options minuet-openai-fim-compatible-options :max_tokens 64))
+
+
+  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+  ;; Codestral
+  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+  (defun my/get-codestral-api-key ()
+    (m/get-password "codestral.ai" "apikey")
+    )
+  (setq minuet-provider 'codestral)
+  (plist-put minuet-codestral-options :api-key #'my/get-codestral-api-key)
+  (minuet-set-optional-options minuet-codestral-options :max_tokens 256)
+
+  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+  ;; GEMINI
+  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+  (setq minuet-provider 'gemini)
+  (plist-put minuet-gemini-options :model "gemini-2.5-flash")
+  (plist-put minuet-gemini-options :api-key #'my/rotate-keys)
+  (minuet-set-optional-options
+   minuet-gemini-options :generationConfig
+   '(:maxOutputTokens 256
+                      :topP 0.9
+                      ;; When using `gemini-2.5-flash`, it is recommended to entirely
+                      ;; disable thinking for faster completion retrieval.
+                      :thinkingConfig (:thinkingBudget 0)))
+
+  (minuet-set-optional-options
+   minuet-gemini-options :safetySettings
+   [(:category "HARM_CATEGORY_DANGEROUS_CONTENT"
+               :threshold "BLOCK_NONE")
+    (:category "HARM_CATEGORY_HATE_SPEECH"
+               :threshold "BLOCK_NONE")
+    (:category "HARM_CATEGORY_HARASSMENT"
+               :threshold "BLOCK_NONE")
+    (:category "HARM_CATEGORY_SEXUALLY_EXPLICIT"
+               :threshold "BLOCK_NONE")])
+
+  (add-hook 'minuet-auto-suggestion-block-functions #'(lambda ()
+                                                        (not (evil-insert-state-p))))
+  )
+
+
+  ;; For Evil users: When defining `minuet-active-mode-map` in insert
+  ;; or normal states, the following one-liner is required.
+
+  ;; (add-hook 'minuet-active-mode-hook #'evil-normalize-keymaps)
+
+  ;; This is *not* necessary when defining `minuet-active-mode-map`.
+
+  ;; To minimize frequent overhead, it is recommended to avoid adding
+  ;; `evil-normalize-keymaps` to `minuet-active-mode-hook`. Instead,
+  ;; bind keybindings directly within `minuet-active-mode-map` using
+  ;; standard Emacs key sequences, such as `M-xxx`. This approach should
+  ;; not conflict with Evil's keybindings, as Evil primarily avoids
+  ;; using `M-xxx` bindings.
+
 
 (provide 'init-ai)
