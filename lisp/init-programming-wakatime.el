77f99e19ac44eb4fa1b8ba1cfaa8694ac9a9f620
Ngoc Nguyen <baongoc124@gmail.com>
2025-08-30 06:56:06 +0900

    use modified wakatime

--- lisp/init-programming-wakatime.el
+++ lisp/init-programming-wakatime.el
@@ -1,7 +1,15 @@
-(use-package wakatime-mode
-  :demand t
-  :diminish wakatime-mode
-  :config
-  (setq wakatime-api-key (m/get-password "wakatime.com" "apikey"))
-  (global-wakatime-mode 1))
+(add-to-list 'load-path "~/.emacs.d/lisp/wakatime-mode/")
+
+(require 'wakatime-mode)
+(setq wakatime-api-key (m/get-password "wakatime.com" "apikey"))
+(global-wakatime-mode 1)
+
+;; (use-package wakatime-mode
+;;   :demand t
+;;   :diminish wakatime-mode
+;;   :config
+;;   (setq wakatime-api-key (m/get-password "wakatime.com" "apikey"))
+;;   (global-wakatime-mode 1)
+;;   )
+
 (provide 'init-programming-wakatime)
